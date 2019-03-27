c4641ef56f7baa6d8f253f9b40a6ecc1990163c8
Megan Miller <35523907+meganmiller98@users.noreply.github.com>
2019-03-27 12:24:34 +0000

    Initial Commit

---
+++ Phoneword/MainActivity.cs
@@ -0,0 +1,54 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using System.Collections.Generic;
+using Android.Content;
+
+namespace Phoneword
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        static readonly List<string> phoneNumbers = new List<string>();
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            // Set our view from the "main" layout resource
+            //hello
+            SetContentView(Resource.Layout.activity_main);
+
+            //Get our UI controls from loaded layout
+            EditText phoneNumberText = FindViewById<EditText>(Resource.Id.PhoneNumberText);
+            TextView translatedPhoneWord = FindViewById<TextView>(Resource.Id.TranslatedPhoneword);
+            Button translateButton = FindViewById<Button>(Resource.Id.TranslateButton);
+            Button translationHistoryButton = FindViewById<Button>(Resource.Id.TranslationHistoryButton); 
+
+            //Add code to translare number
+            translateButton.Click += (sender, e) =>
+            {
+                //Translate user's alphanumeric phone number to numeric
+                string TranslatedNumber = Core.PhonewordTranslator.ToNumber(phoneNumberText.Text);
+                if (string.IsNullOrWhiteSpace(TranslatedNumber))
+                {
+                    translatedPhoneWord.Text = string.Empty;
+                }
+                else
+                {
+                    translatedPhoneWord.Text = TranslatedNumber;
+                    phoneNumbers.Add(TranslatedNumber);
+                    translationHistoryButton.Enabled = true;
+                }
+            };
+
+            translationHistoryButton.Click += (sender, e) =>
+             {
+                 var intent = new Intent(this, typeof(TransaltionHistoryActivity));
+                 intent.PutStringArrayListExtra("phone_numbers", phoneNumbers);
+                 StartActivity(intent);
+             };
+
+        }
+    }
+}
